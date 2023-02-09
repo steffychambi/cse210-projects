4a2c74d4918170ffdb36311d9967f65ae7c8c3cf
steffychambi <steffychambi@gmail.com>
2023-02-09 17:31:41 -0400

    do a while loop for the user to finish the program

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Console.WriteLine("Hello! welcome to Scripture Memorizer");
+        Console.WriteLine("Please press 'enter' to continue or type 'quit' to finish");
+
+        string i = "";
+
+        do
+        {
+
+        Scripture scripture = new Scripture("Moroni 10: 4-5",
+        "And when you receive these things, I would exhort you to a ask God the Eternal Father, in the name of Christ, if these things are not true ; and if you ask with a sincere heart , with a true intent , having faith in Christ, he will manifest to you the truth of them by the power of the Holy Spirit and by the power of the Holy Spirit you will be able to know the truth of all things.");
+        scripture.Display();
+        
+        i = Console.ReadLine();
+        // clear the console
+        Console.Clear();
+
+        }
+        while (i != "quit"); 
     }
 }
