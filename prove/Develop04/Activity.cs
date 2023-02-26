03872b2d7a126cbe2553e8945bf88c2a03de2159
steffychambi <steffychambi@gmail.com>
2023-02-26 02:26:06 -0400

    added showSpinner function

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -4,6 +4,7 @@
         protected string _explanation;
         protected int _duration;
         protected string _outroMessage;
+        private List<string> animationStrings;
         
 
         public Activity(string name,string explanation,int duration)
@@ -23,9 +24,25 @@
 
             Console.WriteLine(_explanation);
         }
-        public void ShowSpinner()
+        public void ShowSpinner(int spinnerDuration)
         {
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(spinnerDuration);
+        animationStrings.Add("|");
+        animationStrings.Add("/");
+        animationStrings.Add("-");
+        animationStrings.Add("\\");
 
+        int index = 0;
+
+        while (DateTime.Now >= endTime)
+        {
+            string s = animationStrings [index];
+            Console.Write(s);
+            Thread.Sleep(100);
+            Console.Write("\b \b");
+            index++;
+        }
         }
 
     }
