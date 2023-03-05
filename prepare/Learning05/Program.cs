0cdb9e829793f0058c5545fb0a87440eff1e88b2
steffychambi <steffychambi@gmail.com>
2023-03-04 21:17:52 -0400

    Program completed

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,23 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("Orange", 5);
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("Green", 6, 8);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("Blue", 4);
+        shapes.Add(circle);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
