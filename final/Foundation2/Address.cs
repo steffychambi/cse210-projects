3c0b691d86b3646d750737c721a9e86ff2007be1
steffychambi <steffychambi@gmail.com>
2023-03-23 18:38:15 -0400

    Address class completed

---
+++ final/Foundation2/Address.cs
@@ -0,0 +1,37 @@
+public class Address
+{
+    private string _street;
+    private string _city;
+    private string _state;
+    private string _country;
+
+    public Address(string street, string city, string state, string country)
+    {
+        _street = street;
+        _city = city;
+        _state = state;
+        _country = country;
+    }
+
+    public bool LiveInUsa()
+    {
+        if(_country == "United States")
+        {
+            return true;
+        }
+        else if(_country == "USA")
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+    }
+
+    public string ReturnAddress()
+    {
+        string AddressString = $"street: {_street}, city: {_city}, state: {_state}, country: {_country}";
+        return AddressString;
+    }
+}
