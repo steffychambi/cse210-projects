47145b0b32c39860fc23727086ad73a9b9f39f3c
steffychambi <steffychambi@gmail.com>
2023-04-01 11:42:52 -0400

    Saving what I have done before

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -3,24 +3,58 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello! welcome to Scripture Memorizer");
-        Console.WriteLine("Please press 'enter' to continue or type 'quit' to finish");
+    {        
 
-        string i = "";
+        string book = "Moroni";
+        string chapter = "10";
+        string firstNumber = "4";
+        string finalNumber = "5";
 
-        do
-        {
-
-        Scripture scripture = new Scripture("Moroni 10: 4-5",
-        "And when you receive these things, I would exhort you to a ask God the Eternal Father, in the name of Christ, if these things are not true ; and if you ask with a sincere heart , with a true intent , having faith in Christ, he will manifest to you the truth of them by the power of the Holy Spirit and by the power of the Holy Spirit you will be able to know the truth of all things.");
-        scripture.Display();
+        // Get the reference of the scripture.
+        Reference reference = new Reference(book, chapter, firstNumber, finalNumber);
+        //string reference = heading.GetHeading();
         
-        i = Console.ReadLine();
-        // clear the console
-        Console.Clear();
+        // Text
+        // string t = "And when you receive these things, I would exhort you to a ask God the Eternal Father, in the name of Christ, if these things are not true; and if you ask with a sincere heart, with a true intent, having faith in Christ, he will manifest to you the truth of them by the power of the Holy Spirit and by the power of the Holy Spirit you will be able to know the truth of all things.";
+        
 
-        }
-        while (i != "quit"); 
+        // Text text = new Text(t);
+        // text.ConvertToList();
+
+        // int totalWords = 0; //text.GetTotalWords();
+        // Console.WriteLine(totalWords);
+
+        // string userDecision = "";
+        // int counter = 0;
+
+        // do
+        // {
+
+        //     if (counter <= totalWords)
+        //     {
+
+        //         // clear the console
+        //         //Console.Clear();
+        //         Console.WriteLine("Scripture Memorizer");
+        //         Console.WriteLine("");
+
+                
+        //         Scripture scripture = new Scripture(reference,t);
+        //         scripture.Display();
+        //         Console.WriteLine("");
+                
+        //         Console.WriteLine("Please press 'enter' to continue or type 'quit' to finish");
+                
+        //         counter += 1;
+        //         t = text.GetNewText();
+        //          userDecision = Console.ReadLine();
+        //     }
+        //     else
+        //     {
+        //         break;
+        //     }
+
+        // }
+        // while (userDecision != "quit"); 
     }
 }
