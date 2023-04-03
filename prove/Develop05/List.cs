2cd39892b760c17e340cdb8b6d776b166d4eb76e
steffychambi <steffychambi@gmail.com>
2023-04-02 22:15:45 -0400

    A method to record events was added

--- prove/Develop05/List.cs
+++ prove/Develop05/List.cs
@@ -9,6 +9,19 @@ public class GoalsList
         return _numGoals;
     }
 
+    public void RecordEvent(int num)
+    {
+        foreach (Goal goal in _list)
+        {
+            if(num == goal.GetGoalNumber())
+            {
+                goal.RecordEvent();
+                Console.WriteLine($"Congratulations!! You have earned {goal.GetPoints()} points!");
+                _gainedPoints += goal.GetPoints();
+            }
+        }
+    }
+
     public void AddGoals(Goal goal)
     {
         _list.Add(goal);
@@ -23,16 +36,8 @@ public class GoalsList
         }
     }
 
-    public void CalculateTotalPoints()
+    public int GetTotalPoints()
     {
-        foreach (Goal goal in _list)
-        {
-            _gainedPoints += goal.GetPoints();
-        }
-    }
-
-    public void ShowTotalPoints()
-    {
-        Console.WriteLine($"You have {_gainedPoints} points");
+        return _gainedPoints;
     }
 }
