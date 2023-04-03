5622d848db7b2b74b1ad8fa818837783d768e8d6
steffychambi <steffychambi@gmail.com>
2023-04-02 20:20:10 -0400

    List class was created

---
+++ prove/Develop05/List.cs
@@ -0,0 +1,31 @@
+public class List
+{
+    private List<Goal> _list = new List<Goal>();
+    private int _gainedPoints;
+
+    public void AddGoals(Goal goal)
+    {
+        _list.Add(goal);
+    }
+
+    public void ShowGoals()
+    {
+        foreach (Goal goal in _list)
+        {
+            goal.ShowGoal();
+        }
+    }
+
+    public void CalculateTotalPoints()
+    {
+        foreach (Goal goal in _list)
+        {
+            _gainedPoints += goal.GetPoints();
+        }
+    }
+
+    public void ShowTotalPoints()
+    {
+        Console.WriteLine($"You have {_gainedPoints} points");
+    }
+}
